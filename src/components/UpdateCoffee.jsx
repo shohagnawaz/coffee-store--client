cc060992868adb3dd38dfa6ef87a294aaca2144f
shohag nawaz <snawazdigital@gmail.com>
2024-05-29 10:36:25 +0600

    addcoffee

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is updateCoffee compo</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
